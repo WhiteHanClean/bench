caca9de5d6ff8b635f18bf5ae3739cf8b36bea3d
Han <whitehanclean@gmail.com>
2021-08-29 16:50:01 +0600

    begin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import MainLayout from "./components/MainLayout";
-
+import Begin from "../src/components/begin/Begin"
 const App = () => {
+
+  const [loading , setLoading ] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2400)
+  }, [])
+
   return (
-    <div>
-      <MainLayout />
-    </div>
+    <>
+    {
+      loading ?
+      <Begin/>
+      :
+      <div>
+        <MainLayout />
+      </div>
+    }
+
+    </>
+
   );
 };
 
---
+++ src/components/begin/Begin.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import begin from '../../assets/begin1.svg'
+import begin2 from '../../assets/begin2.svg'
+import './begin.css'
+
+const Begin = () => {
+    return (
+        <div className="main">
+
+            <img src={begin} class="white_one" alt="" />
+
+            <div className="red_wrapper">
+
+                <div className="red_sub_wrapper">
+                    <img src={begin2} alt="" />
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Begin;
